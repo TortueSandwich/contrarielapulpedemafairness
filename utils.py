a4de092b610fa462d58028ae80acb3ccbadb4895
TortueSandwich <artsaillant@gmail.com>
2025-04-06 10:24:39 +0200

    moitité propre

--- utils.py
+++ utils.py
@@ -6,38 +6,8 @@ import numpy as np
 import math
 from aif360.datasets import BinaryLabelDataset
 import pandas as pd
+from constants import *
 
-
-RNG_SEED = 2025
-MAX_AGE = 120
-sexmap = {"M": "blue", "F": "red"}
-M, F = "M", "F"
-patientid = "Patient ID"
-patientage = "Patient Age"
-patientgender = "Patient Gender"
-# 'OriginalImage[Width','Height]', 'OriginalImagePixelSpacing[x', 'y]',
-viewposition = "View Position"
-
-maladies = [
-    atelectasis := "Atelectasis",
-    cardiomegaly := "Cardiomegaly",
-    consolidation := "Consolidation",
-    edema := "Edema",
-    effusion := "Effusion",
-    emphysema := "Emphysema",
-    fibrosis := "Fibrosis",
-    hernia := "Hernia",
-    infiltration := "Infiltration",
-    mass := "Mass",
-    nofinding := "No Finding",
-    nodule := "Nodule",
-    pleural_thickening := "Pleural_Thickening",
-    pneumonia := "Pneumonia",
-    pneumothorax := "Pneumothorax",
-]
-
-onlymaladies = maladies.copy()
-onlymaladies.remove(nofinding)
 
 # Charger les variables du fichier .env
 def load_env_file(env_path=".env"):
